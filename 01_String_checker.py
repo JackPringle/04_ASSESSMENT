1a5e9e49823b5b34aaf0f74ca2f943a1816d70e5
JackPringle <101750172+JackPringle@users.noreply.github.com>
2023-06-07 11:49:11 +1200

    FINISHED

--- 01_String_checker.py
+++ 01_String_checker.py
@@ -1,14 +1,25 @@
 # String checker function
-
 def string_checker(question, valid_list, error):
     while True:
 
-        # Ask user for choice (and put choice in lowercase)
+        # Ask user for string (and put string in lowercase)
         response = input(question).lower()
 
+        # Check the string is in the valid list
         for item in valid_list:
             if response == item or response == item[0]:
+                print("program continues")
                 return item
 
+        # If not in valid list, print error
         print(error)
         print()
+
+
+# List of valid response
+yes_no_list = ["yes", "no"]
+rtc_list = ["rectangle", "triangle", "circle", "xxx"]
+
+# QUESTION
+played_before = string_checker("R / T / C? ",
+                               rtc_list, "Please enter r / t / c")

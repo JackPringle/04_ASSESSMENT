dd61abf61f27dd106a7e4ac8ea00c98feff867bf
JackPringle <101750172+JackPringle@users.noreply.github.com>
2023-06-22 10:22:14 +1200

    Rearranged

--- 02_Num_checker.py
+++ 02_Num_checker.py
@@ -11,16 +11,16 @@ def num_check(question, num_type, low=None, exit_code=None):
         response = input(question).lower()
 
         try:
-            # Set user response to either an integer or float type
-            response = num_type(response)
-
             # If user enters exit code, return response
             if response == exit_code:
                 return response
 
+            # Set user response to either an integer or float type
+            response = num_type(response)
+
             # If response is less than or equal to low, print error
-            elif low is not None and response <= low:
-                error = "Please enter a number more than 0"
+            if low is not None and response <= low:
+                error = f"Please enter a number more than {low}"
                 print(error)
 
             # Otherwise return response
